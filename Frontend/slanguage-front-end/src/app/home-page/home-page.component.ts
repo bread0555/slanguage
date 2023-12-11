b5cca74a8ecdc17926b9ecf49f4e4393d22a997a
fredheng <frederick.heng@outlook.com>
2023-12-11 18:09:14 +1100

    home screen dynamic with user details

--- Frontend/slanguage-front-end/src/app/home-page/home-page.component.ts
+++ Frontend/slanguage-front-end/src/app/home-page/home-page.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  
+  constructor(public userSerivce: UserService) {}
 
+  percentageCompleted: number = 0
+
+  ngOnInit(): void {
+    this.percentageCompleted = (this.userSerivce.activeUser.Points / 27) * 100
+    console.log(this.percentageCompleted)
+  }
 }
